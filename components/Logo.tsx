229509d1cf68834f969a9575e177853f5529e77f
Tim Etler <tmetler@gmail.com>
2022-06-13 22:31:42 -0700

    Changed logo

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -4,5 +4,9 @@ import React from "react"
 import styles from "./Logo.module.scss"
 
 export const Logo: React.FC = () => {
-  return <div className={styles.logo}>Four Streams Kitchen</div>
+  return (
+    <div className={styles.logo}>
+      <Image src="/logo-white.png" width={400} height={50} />
+    </div>
+  )
 }
