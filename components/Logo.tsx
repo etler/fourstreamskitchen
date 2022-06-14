a0afc5f383470426a4528617af8b5bf594e30e3b
Tim Etler <tmetler@gmail.com>
2022-06-14 13:49:20 -0700

    Added emblem logo

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -3,5 +3,10 @@ import React from "react"
 import styles from "./Logo.module.scss"
 
 export const Logo: React.FC = () => {
-  return <img className={styles.logo} src="/logo-white.png" alt="Four Streams Kitchen" />
+  return (
+    <div className={styles.logoWrapper}>
+      <img className={styles.logo} src="/logo-emblem-small-white.png" alt="四川坊" />
+      <img className={styles.logo} src="/logo-white.png" alt="Four Streams Kitchen" />
+    </div>
+  )
 }
