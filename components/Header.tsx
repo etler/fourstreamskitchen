52448b887d23c5a0e1007bfebe4144b8eb8b972b
Tim Etler <tmetler@gmail.com>
2022-06-17 19:28:50 -0700

    Tweaked header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link"
 import React, { useEffect, useRef } from "react"
+import throttle from "lodash/throttle"
 
 import styles from "./Header.module.scss"
 import { Logo } from "./Logo"
@@ -9,9 +10,9 @@ import PinIcon from "../public/icons/pin.svg"
 
 export const Header: React.FC = () => {
   const headerRef = useRef<HTMLDivElement>(null)
-  const scrollListener: EventListener = () => {
+  const scrollListener: EventListener = throttle(() => {
     headerRef.current?.style.setProperty("--header-scroll-position", `${window.scrollY}`)
-  }
+  }, 100)
   useEffect(() => {
     window.addEventListener("scroll", scrollListener)
     return () => {
@@ -20,41 +21,39 @@ export const Header: React.FC = () => {
   })
   return (
     <>
-      <div className={styles.header} ref={headerRef}>
-        <div className={styles.infoBar}>
-          <a href="tel:8316852121" className={styles.info}>
-            <span className={styles.infoIcon}>
-              <PhoneIcon />
-            </span>
-            <span>(831) 685-2121</span>
-          </a>
-          <a
-            href="https://www.google.com/maps?q=7960+Soquel+Drive,+Aptos,+CA+95003"
-            target="_blank"
-            className={styles.info}
-          >
-            <span className={styles.infoIcon}>
-              <PinIcon />
-            </span>
-            <span>7960 Soquel Drive, Aptos, CA 95003</span>
-          </a>
-        </div>
-        <div className={styles.nav}>
-          <Logo />
-          <div className={styles.linkBar}>
-            <Link href="/order">
-              <a className={styles.link}>Order Online</a>
-            </Link>
-            <Link href="#hours">
-              <a className={styles.link}>Hours</a>
-            </Link>
-            <Link href="#menu">
-              <a className={styles.link}>Menu</a>
-            </Link>
-            <Link href="#location">
-              <a className={styles.link}>Location</a>
-            </Link>
-          </div>
+      <div className={styles.infoBar}>
+        <a href="tel:8316852121" className={styles.info}>
+          <span className={styles.infoIcon}>
+            <PhoneIcon />
+          </span>
+          <span>(831) 685-2121</span>
+        </a>
+        <a
+          href="https://www.google.com/maps?q=7960+Soquel+Drive,+Aptos,+CA+95003"
+          target="_blank"
+          className={styles.info}
+        >
+          <span className={styles.infoIcon}>
+            <PinIcon />
+          </span>
+          <span>7960 Soquel Drive, Aptos, CA 95003</span>
+        </a>
+      </div>
+      <div className={styles.nav} ref={headerRef}>
+        <Logo />
+        <div className={styles.linkBar}>
+          <Link href="/order">
+            <a className={styles.link}>Order Online</a>
+          </Link>
+          <Link href="#hours">
+            <a className={styles.link}>Hours</a>
+          </Link>
+          <Link href="#menu">
+            <a className={styles.link}>Menu</a>
+          </Link>
+          <Link href="#location">
+            <a className={styles.link}>Location</a>
+          </Link>
         </div>
       </div>
     </>
