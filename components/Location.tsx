ae7888b77cb16fee46137175645426a378648c22
Tim Etler <tmetler@gmail.com>
2022-06-17 19:29:25 -0700

    Added location info to location section

--- components/Location.tsx
+++ components/Location.tsx
@@ -6,7 +6,23 @@ import { Section } from "./Section"
 export const Location: React.FC = () => {
   return (
     <Section id="location">
-      <iframe className={styles.location} src="/maps.html"></iframe>
+      <>
+        <div className={styles.section}>
+          <span className={styles.title}>Location</span>
+          <a
+            href="https://www.google.com/maps?q=7960+Soquel+Drive,+Aptos,+CA+95003"
+            target="_blank"
+            className={styles.info}
+          >
+            7960 Soquel Drive, Aptos, CA 95003
+          </a>
+          <span className={styles.title}>Phone</span>
+          <a href="tel:8316852121" className={styles.info}>
+            (831) 685-2121
+          </a>
+        </div>
+        <iframe className={styles.location} src="/maps.html"></iframe>
+      </>
     </Section>
   )
 }
