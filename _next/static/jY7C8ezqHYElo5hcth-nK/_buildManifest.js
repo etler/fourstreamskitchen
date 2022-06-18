e487145d0d85435263353d37ea46fdb489447744
etler <tmetler@gmail.com>
2022-06-18 21:11:02 +0000

    Deploying to gh-pages from  @ 0f6969c590e77f68385b612f7310b8d43c2f0cae 🚀

---
+++ _next/static/jY7C8ezqHYElo5hcth-nK/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/898acd28e3327023.css","static/chunks/pages/index-dddcd4514fb28147.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
