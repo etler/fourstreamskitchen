05d59826df3d59bcc7705982b9f35eddec157bba
Tim Etler <tmetler@gmail.com>
2022-06-28 20:37:54 -0700

    Added google analytics tracking

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,16 @@
-import "../styles/globals.scss"
 import type { AppProps } from "next/app"
 import Head from "next/head"
 
+import { GoogleAnalytics, usePageViews } from "nextjs-google-analytics"
+
+import "../styles/globals.scss"
+
 function MyApp({ Component, pageProps }: AppProps) {
+  usePageViews()
+
   return (
     <>
+      <GoogleAnalytics />
       <Head>
         <meta name="viewport" content="width=375, initial-scale=1.0, maximum-scale=1" />
       </Head>
