924397401073e55f95a584006ac3d532ac7f41ac
Tim Etler <tmetler@gmail.com>
2023-04-14 22:13:33 -0700

    Adjusted hours

--- components/Hours.tsx
+++ components/Hours.tsx
@@ -8,19 +8,19 @@ export const Hours: React.FC = () => {
     <Section id="hours">
       <div className={styles.section}>
         <span className={styles.title}>Open 7 Days a week</span>
-        <span className={styles.hourTitle}>Lunch</span>
-        <div className={styles.hours}>
-          <span className={styles.days}>Monday - Saturday</span>
-          <span className={styles.time}>11:30am - 2:30pm</span>
-        </div>
-        <span className={styles.hourTitle}>Dinner</span>
+        <span className={styles.hourTitle}>Hours</span>
         <div className={styles.hours}>
           <span className={styles.days}>Sunday - Thursday</span>
-          <span className={styles.time}>4:30pm - 9:00pm</span>
+          <span className={styles.time}>11:30pm - 9:00pm</span>
         </div>
         <div className={styles.hours}>
           <span className={styles.days}>Friday - Saturday</span>
-          <span className={styles.time}>4:30pm - 9:30pm</span>
+          <span className={styles.time}>11:30pm - 9:30pm</span>
+        </div>
+        <span className={styles.hourTitle}>Lunch Special</span>
+        <div className={styles.hours}>
+          <span className={styles.days}>Monday - Friday</span>
+          <span className={styles.time}>11:30am - 2:30pm</span>
         </div>
       </div>
     </Section>
